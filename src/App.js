33a831cb07a01d7bba62fee328e7a62ae596033c
Megasonic988 <weixiang@ualberta.ca>
2017-12-17 00:26:09 -0700

    Added image uploading

--- src/App.js
+++ src/App.js
@@ -1,55 +1,104 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { Button, Grid, Row, Col } from 'react-bootstrap';
-
-const forestUrl = "https://vignette.wikia.nocookie.net/tya-universe/images/c/ca/Tangerine-forest.jpg/revision/latest/scale-to-width-down/640?cb=20130818014507";
-const appleUrl = "https://static.pexels.com/photos/39803/pexels-photo-39803.jpeg";
+import { Button, Grid, Row, Col, Navbar } from 'react-bootstrap';
 
 class App extends Component {
 
   constructor() {
     super();
     this.state = {
-      
+      file: null,
+      imageUrl: null
     };
   }
 
   componentDidMount() {
     const canvas = this.refs.canvas;
     const ctx = canvas.getContext("2d");
-    const img = this.refs.image;
+    ctx.rect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = "green";
+    ctx.fill();
+  }
 
-    img.onload = () => {
-      const radius = 1;
+  handleSubmit(e) {
+    e.preventDefault();
+  }
 
-      ctx.drawImage(img, 0, 0, 640, 425);
-      for (let x = 10; x < 640; x = x + 20) {
-        for (let y = 10; y < 425; y = y + 20) {
-          ctx.beginPath();
-          ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
-          ctx.fillStyle = "white";
-          ctx.fill();
-        }
+  handleImageChange(e) {
+    e.preventDefault();
+    const reader = new FileReader();
+    const file = e.target.files[0];
+    reader.onloadend = () => {
+      this.setState({
+        file: file,
+        imageUrl: reader.result
+      });
+    }
+    reader.readAsDataURL(file);
+  }
+
+  calculateCanvasHeight(canvasWidth, imageWidth, imageHeight) {
+    console.log(canvasWidth)
+    return canvasWidth / imageWidth * imageHeight;
+  }
+
+  handleImageLoaded() {
+    const canvas = this.refs.canvas;
+    const image = this.refs.image;
+    canvas.width = canvas.offsetWidth;
+    canvas.height = this.calculateCanvasHeight(canvas.offsetWidth, image.width, image.height);
+    const ctx = canvas.getContext("2d");
+    const radius = 1;
+    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    for (let x = 10; x < canvas.width; x = x + 20) {
+      for (let y = 10; y < canvas.height; y = y + 20) {
+        ctx.beginPath();
+        ctx.arc(x, y, radius, 0, 2 * Math.PI, false);
+        ctx.fillStyle = "white";
+        ctx.fill();
       }
     }
   }
 
+  handleImageLoadError() {
+    console.log("Image Load Error!");
+  }
+
   render() {
+    const { imageUrl } = this.state;
+    let image = null;
+    if (imageUrl) {
+      image = (
+        <img
+          ref="image"
+          src={imageUrl}
+          className="hidden"
+          onLoad={() => this.handleImageLoaded()}
+          onError={() => this.handleImageLoadError()}
+        />
+      );
+    }
     return (
-      <Row>
-        <Col xs={8} className="text-center">
-          <canvas ref="canvas" width={640} height={425} />
-          <img
-            ref="image"
-            src={forestUrl}
-            className="hidden"
-          />
-        </Col>
-        <Col xs={4} className="outline">
-          Labelling Controls
-        </Col>
-      </Row>
+      <div>
+        <Navbar inverse>
+          <Navbar.Header>
+            <Navbar.Brand>
+              CCWJ Image Annotation
+            </Navbar.Brand>
+            <Navbar.Toggle />
+          </Navbar.Header>
+        </Navbar>
+        <Row>
+          <Col md={9} className="text-center">
+            <canvas ref="canvas"/>
+            {image}
+          </Col>
+          <Col md={3}>
+            <input type="file" accept="image/*" onChange={e => this.handleImageChange(e)} />
+          </Col>
+        </Row>
+      </div>
     );
   }
 }
